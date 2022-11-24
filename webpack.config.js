931f98427843bb08e0bd546b41aea16d4e320ec1
Omar Lahouaichri <lahouaichri.omar@gmail.com>
2022-11-24 12:35:44 +0100

    creation controller and view legal

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,7 @@ Encore
 
     /*CSS*/
     .addEntry('headerCss', './assets/styles/header.scss')
+    .addEntry('legalCss', './assets/styles/legal.scss')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
@@ -73,15 +74,15 @@ Encore
     // uncomment if you use TypeScript
     // .enableTypeScriptLoader()
 
-// uncomment if you use React
-// .enableReactPreset()
+    // uncomment if you use React
+    // .enableReactPreset()
 
-// uncomment to get integrity="..." attributes on your script & link tags
-// requires WebpackEncoreBundle 1.4 or higher
-// .enableIntegrityHashes(Encore.isProduction())
+    // uncomment to get integrity="..." attributes on your script & link tags
+    // requires WebpackEncoreBundle 1.4 or higher
+    // .enableIntegrityHashes(Encore.isProduction())
 
-// uncomment if you're having problems with a jQuery plugin
-// .autoProvidejQuery()
+    // uncomment if you're having problems with a jQuery plugin
+    // .autoProvidejQuery()
 
     // enables Sass/SCSS support
     .enableSassLoader();
